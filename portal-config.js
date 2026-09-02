07494db40ecbd1c136beb63a0109c26522f5c5cd
sundeepshub <sunny.great12@gmail.com>
2026-09-03 00:52:47 +0530

    Add files via upload

--- portal-config.js
+++ portal-config.js
@@ -1,1 +1,1 @@
-window.HC_PORTAL_CONFIG={appsScriptUrl:'https://script.google.com/macros/s/AKfycbwXcFXo-rdrcxWBqkzSWXhU_KYUShb3wwq7wNjV3bMThjHiCg7ELSoDU0cqqIj3atXZmQ/exec'};
+window.HC_PORTAL_CONFIG={appsScriptUrl:'https://script.google.com/macros/s/AKfycbzkoa_wXH4QKtEfHu--vYQlfF3oI27W-9YFG5VJcZ-g6Hh4BxGopGfyg3JCS1FwVZHh9w/exec'};
